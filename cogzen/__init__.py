21fab8c9056505786e2745a3165cd47d6e062b8e
cogsys-io <113848740+cogsys-io@users.noreply.github.com>
2025-03-11 23:30:54 +0100

    init

--- cogzen/__init__.py
+++ cogzen/__init__.py
@@ -13,6 +13,29 @@ __author__ = """cogsys.io"""
 __email__ = "cogsys@cogsys.io"
 
 
+from . import aux_sys
+from .aux_sys import chdir
+from .aux_sys import pushdir
+from .aux_sys import pushdir as pdir
+
+from . import aux_log  # noqa: F401
+from .aux_log import Log0
+
+from . import aux_str
+from .aux_str import clean_str
+from .aux_str.now import now
+
+from . import aux_pandas
+from .aux_pandas import disp_df
+from .aux_pandas import repr_df
+from .aux_pandas import disp_df as ddf
+from .aux_pandas import repr_df as rdf
+
+from . import aux_srsly
+from .aux_srsly import jsonable
+from .aux_srsly import yamlstr
+
+
 def get_module_version():
     return __version__
 
